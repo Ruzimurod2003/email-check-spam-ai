ec9f756440d0275fb28bcdefbaab3264ca27ff31
Rozimurod Abdunazarov <rozimurod.abdunazarov@esynergy.lv>
2024-12-07 20:39:04 +0500

    added new comments

--- program.py
+++ program.py
@@ -7,12 +7,15 @@ from sklearn.pipeline import Pipeline
 import warnings
 
 warnings.filterwarnings("ignore")
+
+# Ma'lumotlarni yuklash va tayyorlash
 df = pd.read_csv("spam.csv")
 df["Spam"] = df["Category"].apply(lambda x: 1 if x == "spam" else 0)
-df_spam = df[df["Category"] == "spam"].copy()
+
+# Mashg'ulot va test to'plamini ajratish
 X_train, X_test, y_train, y_test = train_test_split(df.Message, df.Spam, test_size=0.25)
 
-
+# Modelni baholash funksiyasi
 def evaluate_model(model, X_train, X_test, y_train, y_test):
     model.fit(X_train, y_train)
     y_pred_train = model.predict(X_train)
@@ -30,24 +33,18 @@ def evaluate_model(model, X_train, X_test, y_train, y_test):
     F1_train = cr_train["weighted avg"]["f1-score"]
     F1_test = cr_test["weighted avg"]["f1-score"]
     model_score = [
-        precision_train,
-        precision_test,
-        recall_train,
-        recall_test,
-        acc_train,
-        acc_test,
-        roc_auc_train,
-        roc_auc_test,
-        F1_train,
-        F1_test,
+        precision_train, precision_test, recall_train, recall_test,
+        acc_train, acc_test, roc_auc_train, roc_auc_test, F1_train, F1_test,
     ]
     return model_score
 
-
+# Model yaratish
 clf = Pipeline([("vectorizer", CountVectorizer()), ("nb", MultinomialNB())])
 
+# Model bahosi
 MultinomialNB_score = evaluate_model(clf, X_train, X_test, y_train, y_test)
 
+# Spamni aniqlash funksiyasi
 def detect_spam(email_text):
     prediction = clf.predict([email_text])
     if prediction == 0:
@@ -55,7 +52,7 @@ def detect_spam(email_text):
     else:
         return "This is a Spam Email!"
 
-
+# Sinov e-mail matnlar
 sample_email = "Congratulations! You have WON a $1000 Gift Card!"
 result = detect_spam(sample_email)
 print(result)
